025bbf7527f38b0e8edb7b77e1e61b9932e0c11c
LordTejas <tejas5225@gmail.com>
2023-06-04 00:57:20 +0530

    FRONTEND INIT

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,13 @@
+interface IHome {
+    
+}
+
+const Home = (props: IHome) => {
+    return (
+        <main>
+            <h1 className="text-4xl font-semibold text-center">Home</h1>
+        </main>
+    );
+}
+
+export default Home;
